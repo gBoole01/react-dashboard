3490a6b188c9d11bfa7a4f8f67c3e0ae2d58d4a0
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-08 18:04:07 +0200

    feat: add Pie scene

--- src/scenes/pie/index.jsx
+++ src/scenes/pie/index.jsx
@@ -1,14 +1,18 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import PieChart from "../../components/PieChart";
 
-const Pie = () => {
+const Bar = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+        <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      </Box>
+      <Box height="70vh">
+        <PieChart />
       </Box>
     </Box>
   );
 };
 
-export default Pie;
+export default Bar;
