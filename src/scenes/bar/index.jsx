8691af13e5bc5460cbd82fa920dd27e0095b886e
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-08 18:00:11 +0200

    feat: add Bar scene

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import BarChart from "../../components/BarChart";
 
 const Bar = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+        <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      </Box>
+      <Box height="70vh">
+        <BarChart />
       </Box>
     </Box>
   );
